09717fb5f6bf16b05316cb2532278fa61f8f1f4d
colin <colin.williams.dev@gmail.com>
2023-07-13 14:27:26 -0500

    feature: discord projects channel event listener

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { sendTest, sendTwilioSMS } from "./twilio.js";
 import members from './members/index.js';
 
 
-
 /**
  *  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  * ^^^^^^^^^^ EVENT LISTENERS ^^^^^^^^^^
@@ -27,22 +26,33 @@ discord.on("ready", (bot) => {
   console.log(`✅ ${bot.options.rest.authPrefix} ${bot.user.tag} is online! Listening to channels: ${bot.channels}`);
 });
 
+/**
+ * @description use `message.channel` to validate channel from channel data
+ */
 discord.on("messageCreate", (message) => {
   /* this validation disallows bots from responding to each other/themselves, remove at your own risk 💀 */
   if (message.author.bot) return;
 
+  console.log("LOOK HERE", message.createdAt + 24);
+
+  if (message.channel.parentId === "1126197975983591534" && message.channel.messageCount === 1) {
+    const projectName = message.channel.name;
+    const projectOwner = message.author.username;
+    const projectCreatedAt = message.createdAt;
+    // sendTwilioSMS(projectName, projectOwner, projectCreatedAt);
+    console.log("WE MADE IT!", projectName, projectOwner, projectCreatedAt);
+    message.reply("( ͡° ͜ʖ ͡°)");
+  }
+
   /* message sent in server from any user: */
   console.log(`Discord message: "${message.content}" from User: ${message.author.username} at ${message.createdAt}`);
+  console.log("CHANGE NAME:", message.author, message);
 
   /* bot will react to any message sent with this emoji */
-  message.react('🤓');
+  // message.react('🤓');
 });
 
 discord.login(process.env.TOKEN);
 
 // WORKS!
 // sendTwilioSMS();
-
-// TODO Establish Discord Listen to "Projects" forum channel "new post" event
-// TODO Hook up sendTwilioSMS fn to Discord Event Listener
-
