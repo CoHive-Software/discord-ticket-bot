fc1711aee6631d3b26d9e446d346db93b40628db
colin <colin.williams.dev@gmail.com>
2023-07-14 16:49:50 -0500

    add: more members

--- src/members/index.js
+++ src/members/index.js
@@ -5,11 +5,13 @@ const members = [
   { name: "Benjamin Lyon", email: "carlbenjaminlyon@gmail.com", phone: "+19859519956" },
   { name: "Matthew Mekita", email: "mpmekita@gmail.com", phone: "+12016022084" },
   { name: "Jessica Torres", email: "jessicatorres.debug@gmail.com", phone: "+15046212549" },
-  // { name: "Jordan Mann", email: "jordan.mann.dev@gmail.com", phone: "+15713191640" },
   { name: "Andrew Vasquez", email: "andrewvasquez.dev@gmail.com", phone: "+17609754217" },
   { name: "Jorge Carvajal", email: "", phone: "" },
-  { name: "Neil Shapiro", email: "neilshapiro.dev@gmail.com", phone: "+15043399511" }
-  // { name: "Marcus Ager", email: "marcus.ager.dev@gmail.com ", phone: "+15045648891" }
+  { name: "Neil Shapiro", email: "neilshapiro.dev@gmail.com", phone: "+15043399511" },
+  { name: "Benjamin Dickinson", email: "bsadickinson@gmail.com", phone: "" },
+  { name: "Sam Littell", email: "samlittellmds@gmail.com", phone: "+15124963213" },
+  // { name: "Jordan Mann", email: "jordan.mann.dev@gmail.com", phone: "+15713191640" },
+  // { name: "Marcus Ager", email: "marcus.ager.dev@gmail.com", phone: "+15045648891" }
 ];
 
 export default members;
